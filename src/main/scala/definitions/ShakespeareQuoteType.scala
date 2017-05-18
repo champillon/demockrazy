bd26d6b8bc802165ed1aee97122be9b56d2f9866
champillon <passapong.t@gmail.com>
2017-05-18 15:48:17 +0700

    created

---
+++ src/main/scala/definitions/ShakespeareQuoteType.scala
@@ -0,0 +1,15 @@
+package definitions
+
+/**
+  * Created by champillon on 5/18/2017 AD.
+  */
+object ShakespeareQuoteType
+  extends Enumeration {
+  type ShakespeareQuoteType = Value
+
+  val ROMEO_AND_JULIET = Value(0)
+  val HAMLET = Value(1)
+  val AS_YOU_LIKE_IT = Value(2)
+  val KING_RICHARD_III = Value(3)
+
+}
